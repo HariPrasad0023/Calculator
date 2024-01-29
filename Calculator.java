db00e065d601a25ac0aecf4f9cee49d9c9c77824
HariPrasad0023 <C.HariPrasad@iiitb.ac.in>
2024-01-29 19:45:38 +0530

    Sample Calculator program

---
+++ Calculator.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Calculator {
+	public static void main(String args[]) {
+		float var1, var2, result;
+		int choice;
+
+		Scanner scan = new Scanner(System.in);
+
+		do {
+			System.out.println ("1. Add");
+			System.out.println ("2. Subtract");
+			System.out.println ("3. Multiply");
+			System.out.println ("4. Divide");
+			System.out.println ("5. Exit");
+
+			System.out.print ("Your choice : ");
+			choice = scan.nextInt();
+
+			if (choice == 5 ) System.exit(0);
+
+			System.out.print ("1st Number : ");
+			var1 = scan.nextInt();
+
+			System.out.print ("2nd Number : ");
+			var2 = scan.nextInt();
+
+			switch(choice) {
+				case 1: System.out.println (var1 + " + " + var2 + " = " + (var1 + var2));
+						break;
+				case 2: System.out.println (var1 + " - " + var2 + " = " + (var1 - var2));
+						break;
+				case 3: System.out.println (var1 + " * " + var2 + " = " + (var1 * var2));
+						break;
+				case 4: if (var2 == 0) {
+							System.out.println ("Divide by Zero error");
+							System.exit(0);
+				 		}
+						System.out.println (var1 + " / " + var2 + " = " + (var1 / var2));
+						break;
+				default: System.out.println ("Please select the correct choice...");
+			}
+			System.out.println ("---------------------------------------------------------------");
+		} while (true);
+	}
+}
