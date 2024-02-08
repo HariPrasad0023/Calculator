6a014b9f41f37519f1f20c4195ac29f35c8388ae
HariPrasad0023 <C.HariPrasad@iiitb.ac.in>
2024-02-08 10:05:53 +0530

    removed recursive loop

--- Calculator.java
+++ Calculator.java
@@ -1,55 +1,44 @@
-import java.util.Scanner;
-
 public class Calculator {
 	public static void main(String args[]) {
-		float var1, var2, result;
+		float var1, var2;
 		int choice;
 
-		Scanner scan = new Scanner(System.in);
+		System.out.println("1. Add");
+		System.out.println("2. Subtract");
+		System.out.println("3. Multiply");
+		System.out.println("4. Divide");
 
-		do {
-			System.out.println("1. Add");
-			System.out.println("2. Subtract");
-			System.out.println("3. Multiply");
-			System.out.println("4. Divide");
-			System.out.println("5. Exit");
+		System.out.println("Your choice : ");
+		// choice = scan.nextInt();
+		choice = Integer.parseInt(args[0]);
 
-			System.out.print("Your choice : ");
-			// choice = scan.nextInt();
-			choice = Integer.parseInt(args[0]);
+		System.out.println("1st Number : ");
+		// var1 = scan.nextInt();
+		var1 = Integer.parseInt(args[1]);
 
-			if (choice == 5)
-				System.exit(0);
+		System.out.println("2nd Number : ");
+		// var2 = scan.nextInt();
+		var2 = Integer.parseInt(args[2]);
 
-			System.out.print("1st Number : ");
-			// var1 = scan.nextInt();
-			var1 = Integer.parseInt(args[1]);
-
-			System.out.print("2nd Number : ");
-			// var2 = scan.nextInt();
-			var2 = Integer.parseInt(args[2]);
-
-			switch (choice) {
-				case 1:
-					System.out.println(var1 + " + " + var2 + " = " + (var1 + var2));
-					break;
-				case 2:
-					System.out.println(var1 + " - " + var2 + " = " + (var1 - var2));
-					break;
-				case 3:
-					System.out.println(var1 + " * " + var2 + " = " + (var1 * var2));
-					break;
-				case 4:
-					if (var2 == 0) {
-						System.out.println("Divide by Zero error");
-						System.exit(0);
-					}
-					System.out.println(var1 + " / " + var2 + " = " + (var1 / var2));
-					break;
-				default:
-					System.out.println("Please select the correct choice...");
-			}
-			System.out.println("---------------------------------------------------------------");
-		} while (true);
+		switch (choice) {
+			case 1:
+				System.out.println(var1 + " + " + var2 + " = " + (var1 + var2));
+				break;
+			case 2:
+				System.out.println(var1 + " - " + var2 + " = " + (var1 - var2));
+				break;
+			case 3:
+				System.out.println(var1 + " * " + var2 + " = " + (var1 * var2));
+				break;
+			case 4:
+				if (var2 == 0) {
+					System.out.println("Divide by Zero error");
+					System.exit(0);
+				}
+				System.out.println(var1 + " / " + var2 + " = " + (var1 / var2));
+				break;
+			default:
+				System.out.println("Please select the correct choice...");
+		}
 	}
 }
